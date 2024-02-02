b392c669d5c441ca516bfddcb2e56637bd51377f
Adam2005Prado <adamfap2021@gmail.com>
2024-02-02 14:51:47 -0300

    atualizado

--- aula2/Frmcalculadora.cs
+++ aula2/Frmcalculadora.cs
@@ -10,6 +10,18 @@ namespace aula2
         //
         private void button1_Click_1(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox2.Focus();
+                return;
+            }
             Int32 valor1 = Convert.ToInt32(textBox1.Text);
             Int32 valor2 = Convert.ToInt32(textBox2.Text);
             Int32 soma = valor1 + valor2;
@@ -17,6 +29,18 @@ namespace aula2
         }
         private void button2_Click_1(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox2.Focus();
+                return;
+            }
             Int32 valor1 = Convert.ToInt32(textBox1.Text);
             Int32 valor2 = Convert.ToInt32(textBox2.Text);
             Int32 subtracao = valor1 - valor2;
@@ -24,6 +48,18 @@ namespace aula2
         }
         private void button3_Click_1(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox2.Focus();
+                return;
+            }
             Int32 valor1 = Convert.ToInt32(textBox1.Text);
             Int32 valor2 = Convert.ToInt32(textBox2.Text);
             Int32 multiplicacao = valor1 * valor2;
@@ -31,6 +67,18 @@ namespace aula2
         }
         private void button4_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox2.Focus();
+                return;
+            }
             float valor1 = Convert.ToInt32(textBox1.Text);
             float valor2 = Convert.ToInt32(textBox2.Text);
             float divisao = valor1 / valor2;
@@ -39,12 +87,36 @@ namespace aula2
 
         private void button5_Click(object sender, EventArgs e)
         {
+            if (string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox2.Focus();
+                return;
+            }
             double raiz = Convert.ToDouble(textBox1.Text);
             MessageBox.Show("Raiz :" + Math.Sqrt(raiz));
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
+            if(string.IsNullOrEmpty(textBox1.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox1.Focus();
+                return;
+            }
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                MessageBox.Show("campo vazio");
+                textBox2.Focus();
+                return;
+            }
             double valor= Convert.ToDouble(textBox1.Text);
             double elevado = Convert.ToDouble(textBox2.Text);
             MessageBox.Show("potencia " + Math.Pow(valor, elevado));
