02902c912163c86e4a525c1c9bf6fac61fceaa4a
JoaopedroSantana1 <102034191+JoaopedroSantana1@users.noreply.github.com>
2024-02-02 13:41:47 -0300

    aula2

---
+++ aula2/Frmcalculadora.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace aula2
+{
+    partial class Frmcalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frmcalculadora));
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            button5 = new Button();
+            button6 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(77, 201);
+            button1.Name = "button1";
+            button1.Size = new Size(197, 90);
+            button1.TabIndex = 2;
+            button1.Text = "Soma";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click_1;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(322, 201);
+            button2.Name = "button2";
+            button2.Size = new Size(173, 90);
+            button2.TabIndex = 3;
+            button2.Text = "Subtração";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click_1;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(123, 337);
+            button3.Name = "button3";
+            button3.Size = new Size(109, 29);
+            button3.TabIndex = 4;
+            button3.Text = "Multiplicação";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click_1;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(375, 337);
+            button4.Name = "button4";
+            button4.Size = new Size(94, 29);
+            button4.TabIndex = 5;
+            button4.Text = "Divisão";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Location = new Point(66, 70);
+            label1.Name = "label1";
+            label1.Size = new Size(130, 20);
+            label1.TabIndex = 7;
+            label1.Text = "Digite um número";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Location = new Point(401, 83);
+            label2.Name = "label2";
+            label2.Size = new Size(145, 20);
+            label2.TabIndex = 6;
+            label2.Text = "Digite outro número";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(202, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(565, 76);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 1;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Transparent;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(596, 351);
+            button5.Name = "button5";
+            button5.Size = new Size(94, 29);
+            button5.TabIndex = 8;
+            button5.Text = "Raiz";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.Transparent;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(582, 214);
+            button6.Name = "button6";
+            button6.Size = new Size(94, 29);
+            button6.TabIndex = 9;
+            button6.Text = "Potência";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // Frmcalculadora
+            // 
+            BackColor = Color.IndianRed;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(792, 425);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            DoubleBuffered = true;
+            Name = "Frmcalculadora";
+            StartPosition = FormStartPosition.CenterParent;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Button button5;
+        private Button button6;
+    }
+}
---
+++ aula2/Frmcalculadora.cs
@@ -0,0 +1,53 @@
+
+namespace aula2
+{
+    public partial class Frmcalculadora : Form
+    {
+        public Frmcalculadora()
+        {
+            InitializeComponent();
+        }
+        //
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Int32 valor1 = Convert.ToInt32(textBox1.Text);
+            Int32 valor2 = Convert.ToInt32(textBox2.Text);
+            Int32 soma = valor1 + valor2;
+            MessageBox.Show("Resultado :" + soma.ToString());
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            Int32 valor1 = Convert.ToInt32(textBox1.Text);
+            Int32 valor2 = Convert.ToInt32(textBox2.Text);
+            Int32 subtracao = valor1 - valor2;
+            MessageBox.Show("Resultado :" + subtracao.ToString());
+        }
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+            Int32 valor1 = Convert.ToInt32(textBox1.Text);
+            Int32 valor2 = Convert.ToInt32(textBox2.Text);
+            Int32 multiplicacao = valor1 * valor2;
+            MessageBox.Show("Resultado :" + multiplicacao.ToString());
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            float valor1 = Convert.ToInt32(textBox1.Text);
+            float valor2 = Convert.ToInt32(textBox2.Text);
+            float divisao = valor1 / valor2;
+            MessageBox.Show("Resultado :" + divisao.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double raiz = Convert.ToDouble(textBox1.Text);
+            MessageBox.Show("Raiz :" + Math.Sqrt(raiz));
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            double valor= Convert.ToDouble(textBox1.Text);
+            double elevado = Convert.ToDouble(textBox2.Text);
+            MessageBox.Show("potencia " + Math.Pow(valor, elevado));
+        }
+    }
+}
