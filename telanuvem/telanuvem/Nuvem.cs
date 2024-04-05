923dd7fc4a476ed7786c0ed299967e2bf5ccfa59
Adam2005Prado <125375073+Adam2005Prado@users.noreply.github.com>
2024-04-05 11:06:25 -0300

    telanuvem

---
+++ telanuvem/telanuvem/Nuvem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace telanuvem
+{
+    public class Nuvem
+    {
+        public static string getproductdescription(int choice)
+        {
+            switch (servico) {
+            case 1: " Microsoft Word  Versão 2311 (Build 17029.20108)- R$ 791,2 ";
+                    case 2: "Microsoft Powerpoint 2019"
+                        }
+        }
+    }
+}
